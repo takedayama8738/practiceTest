5fd4fa760269eda11200f56cdb63dd72b6385168
takedayama8738 <108208574+takedayama8738@users.noreply.github.com>
2022-06-26 15:37:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-docment.getElementById("baz").addEventListener("click", function(){
+document.getElementById("baz").addEventListener("click", function(){
     aleat('ナイスクリック　１回目');
     aleat('ナイスクリック　２回目');
 });
